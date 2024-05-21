985d6c09d61cd1bdfdf9b199e160cb08c9bab335
Hack-it-Rafi <zozorafi2016@gmail.com>
2024-05-21 17:41:35 +0600

    Modular appoach implemented

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { ProductRoutes } from './app/modules/Product/product.route';
+const app: Application = express();
+// const port = 3000
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/products', ProductRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Go to '/api/products' for products, '/api/orders' for orders");
+});
+
+export default app;
